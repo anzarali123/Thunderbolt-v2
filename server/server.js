73412879f200e9ae73f593d8674f4e5b884f3f4e
Anzar Ali <anzarali150799@gmail.com>
2022-11-07 11:57:54 +0530

    added procfile

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const colors = require("colors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,6 +17,20 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
